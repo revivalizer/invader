3269758266003e846748129571b2e210bdc3b6b5
Ralph Brorsen <ralph@deadfeed.net>
2014-07-23 22:04:27 +0200

    First steps towards better dot operator, type system with methods.

---
+++ compiler/types.lua
@@ -0,0 +1,17 @@
+types = {
+	"num" = {
+		methods = {
+		}
+	},
+	"sample" = {
+		methods = {
+			{
+				["name"] = "ADSR",
+				["id"] = 0x101,
+				["arguments"] = {"num", "num", "num", "num"},
+				["return_type"] = "sample",
+				["description"] = "Basic ADSR envelope.",
+			},
+		}
+	},
+}
